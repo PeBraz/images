f2d6a5abdd079fcb7260ebca6ac7079ad26d1e98
pebraz <pbraz.93@gmail.com>
2019-11-12 22:10:14 +0100

    added scripts

---
+++ scripts/get-data.js
@@ -0,0 +1,17 @@
+import request from 'request';
+import fs from 'fs';
+
+const DATA_URL = 'https://s3.amazonaws.com/static.operam.com/assignment/structure_released.xml';
+const OUTPUT_FILE = './output/plants.xml'
+
+request(DATA_URL, (err, res, body) => {
+  if (err) throw err;
+  if (!res || res.statusCode !== 200 || !body) {
+    console.warn('Failed to load URL');
+    return;
+  }
+  fs.writeFile(OUTPUT_FILE, body, {flag: 'w+'}, err => {
+    if (err) throw err;
+    console.log('FILE SAVED');
+  });
+});
---
+++ scripts/store-data.js
@@ -0,0 +1,42 @@
+import { parseString } from 'xml2js';
+import fs from 'fs';
+import betterSqlite3 from 'better-sqlite3';
+
+const db = betterSqlite3('./plants.db');
+const OUTPUT_FILE = './output/plants.xml'
+
+function parseContent(content, parentName = null) {
+  const results = [];
+  if (!content) {
+    return results;
+  }
+  const name = content['$'].words;
+  const fullName = parentName ? `${parentName} > ${name}` : name ;
+  for (const row of (content.synset) || []) {
+    for (const result of parseContent(row, fullName)) {
+      results.push(result);
+    }
+  }
+  results.push({name: fullName, size: results.length});
+  return results;
+}
+
+db.prepare('CREATE TABLE IF NOT EXISTS plants (name TEXT, size NUMBER)').run();
+
+const data = fs.readFileSync(OUTPUT_FILE, 'utf-8');
+parseString(data, (err, res) => {
+  if (err) {
+    throw err;
+  }
+  const startContent = Object.values(res)[0].synset[0];
+  const all = parseContent(startContent);
+
+  const stmt = db.prepare('INSERT INTO plants VALUES (?, ?)')
+  db.transaction(values => {
+    for (const {name, size} of values) {
+      stmt.run(name, size);
+    }
+  })(all);
+});
+console.log('Finito');
+db.close();
